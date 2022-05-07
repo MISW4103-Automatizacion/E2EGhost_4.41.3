0d189b80ffe12d1b6a72f2b34fa350ad36b338c8
mcgomeztuniandes <mc.gomezt1@uniandes.edu.co>
2022-05-06 21:18:26 -0500

    Se agregan nuevos escenarios para el staff

---
+++ cypress/test/cypress/integration/modificar_staff_scenario11_spect.js
@@ -0,0 +1,35 @@
+describe('Escenario 11 Crear un staff de tipo Administrador y validar que aparezca en la lista de staff, eliminarlos y validar que ya no aparezca en la lista', () => {
+    const loginUser = require('../funcionalidades/ingresarLogin')
+    const registerUser = require('../funcionalidades/registrarUsuario')
+    const salirAplicacion = require('../funcionalidades/salirAplicacion')
+    const staffAplicacion = require('../funcionalidades/staffAplicacion')
+    const staffAplicacionCrear = require('../funcionalidades/staffAplicacionCrear')
+    const staffAplicacionBuscar = require('../funcionalidades/staffAplicacionBuscar')
+    const staffAplicacionEliminar = require('../funcionalidades/staffAplicacionEliminar')
+    const { faker } = require('@faker-js/faker')
+    
+    beforeEach(()=> {
+      cy.clearCookies()
+      cy.visit('/')
+      cy.get('main').then(($main) => {
+        if($main.find('form').length > 0) {
+          if($main.find('form')[0].id == 'setup') {
+            registerUser.registerUser(cy, Cypress.env('NAMEBLOG'), Cypress.env('FULLNAME'), Cypress.env('USER'), Cypress.env('PASSWORD'))
+            salirAplicacion.salirAplicacion(cy)
+          }
+        }
+      })
+      loginUser.loginUser(cy, Cypress.env('USER'), Cypress.env('PASSWORD'))
+    })
+    
+    it('Eliminar un staff de tipo Administrador', () => {
+      let mail = faker.internet.email()
+      staffAplicacion.staffAplicacion(cy)
+      staffAplicacionCrear.staffAplicacionCrear(cy, mail, 'Administrador')
+      staffAplicacion.staffAplicacion(cy)
+      staffAplicacionBuscar.staffAplicacionBuscar(cy, mail, true)
+      staffAplicacionEliminar.staffAplicacionEliminar(cy, mail)
+      staffAplicacionBuscar.staffAplicacionBuscar(cy, mail, false)
+      salirAplicacion.salirAplicacion(cy)
+    })
+  })
