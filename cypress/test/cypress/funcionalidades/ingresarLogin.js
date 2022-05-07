089222715636fc711cb1630acefa09a234740aef
mcgomeztuniandes <mc.gomezt1@uniandes.edu.co>
2022-05-07 01:57:20 -0500

    Se genera un time

--- cypress/test/cypress/funcionalidades/ingresarLogin.js
+++ cypress/test/cypress/funcionalidades/ingresarLogin.js
@@ -2,6 +2,7 @@ const loginUser = function (cy, email, password) {
     cy.get('#ember7').type(email)
     cy.get('#ember9').type(password)
     cy.get('#ember11').click()
+    cy.wait(3000)
     cy.get('h2.gh-canvas-title').should('contain', 'Dashboard')
 };
 module.exports = {loginUser: loginUser};
